4e8516f14cbe95b90a9661de299bad028f5fe875
ivanenkorostik <ivanenkor_rostislav_@example.com>
2026-05-08 08:54:42 +0300

    version 0.1.2

--- src/components/routepanel/routepanel.tsx
+++ src/components/routepanel/routepanel.tsx
@@ -1,28 +1,33 @@
-import './routepanel.css'
-import RouteInfo from '../routeinfo/routeInfo' 
-
-
+import './routepanel.css';
+import RouteInfo from '../routeinfo/routeInfo';
 
 function RoutePanel() {
-    return (
-        <section className="routePanel">
-            <div className="fields">
-                <div className="field-start">
-                    <label htmlFor="start">Start Location</label>
-                    <input type="text" placeholder="Start Location" />
-                </div>
-                <div className="field-end">
-                    <label htmlFor="end">End Location</label>
-                    <input type="text" placeholder="End Location" />
-                </div>
-                <div className="field-fuel">
-                    <label htmlFor="fuel">Fuel Consumption</label>
-                    <input type="text" placeholder="fuel consumption" />
-                </div>
-                <button className='btn-calc'>Створити марщрут</button>
-                <RouteInfo />
-            </div>
+  return (
+    <section className="routePanel" aria-label="Route planner">
+      <form className="routePanel_form">
+        <div className="routePanel_field">
+          <label htmlFor="start">Звідки</label>
+          <input id="start" type="text" placeholder="Початкова локація" />
+        </div>
+
+        <div className="routePanel_field">
+          <label htmlFor="end">Куди</label>
+          <input id="end" type="text" placeholder="Кінцева локація" />
+        </div>
+
+        <div className="routePanel_field">
+          <label htmlFor="fuel">Витрата пального</label>
+          <input id="fuel" type="number" min="0" step="0.1" placeholder="л / 100 км" />
+        </div>
+
+        <button className="routePanel_button" type="button">
+          Створити маршрут
+        </button>
+      </form>
+
+      <RouteInfo />
         </section>
-    )
+  );
 }
+
 export default RoutePanel;
